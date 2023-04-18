33f2913009fe310edd9897e63b20e77850029b5b
thomillaz <kamomillathomaz@gmail.com>
2023-04-18 10:54:02 -0300

    add estilo pra shadow

--- src/cardNews.js
+++ src/cardNews.js
@@ -15,8 +15,14 @@ class Cardnews extends HTMLElement {
         cardLeft.setAttribute("class", "card__left");
 
         const autor = document.createElement("span");
-        const linkTitlte = document.createElement("a");
+        autor.textContent = 'By ' + (this.getAttribute('autor') || "Anonymous");
+
+        const linkTitle = document.createElement("a");
+        linkTitle.textContent = this.getAttribute('title');
+        linkTitle.href = this.getAttribute('link-url');
+
         const newsContent = document.createElement("p");
+        newsContent = this.getAttribute('content');
 
         cardLeft.appendChild(autor);
         cardLeft.appendChild(linkTitle);
@@ -26,6 +32,8 @@ class Cardnews extends HTMLElement {
         cardRight.setAttribute("class", "card__right");
 
         const newsImage = createElement("img");
+        newsImage.src = (this.getAttribute('photo') || './img/photo.webp');
+        newsImage.alt = 'Foto da notícia';
         cardRight.appendChild(newsImage);
 
         componentRoot.appendChild(cardLeft);
@@ -34,7 +42,44 @@ class Cardnews extends HTMLElement {
         return componentRoot;
     }
 
-    styles() { }
+    styles() {
+        const style = document.createElement('style');
+        style.textContent = `
+            .card{
+                width: 720px;
+                margin: 10px auto;
+                display: flex;
+                justify-content: space-between;
+                box-shadow: 1px 1px 10px #00000066, -1px -1px 10px #00000066;
+            }
+            .card__left{
+                width: 60%;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                padding: 20px 0 0 20px;
+            }
+            .card__left span{
+                font-weight: bolder;
+            }
+            .card__left a{
+                font-size: 30px;
+                margin: 15px 0 5px;
+                color: #333;
+            }
+            .card__left p{
+                color: #666;
+            }
+            .card__right{
+                width: 40%;
+            }
+            .card__right img{
+                width: 100%;
+            }
+        `
+
+        return style;
+    }
 }
 
 customElements.define("card-news", Cardnews);
